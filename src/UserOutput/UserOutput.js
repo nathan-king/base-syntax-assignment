6bdcfb509f5af21d61fed3a0e2f88efc14bbaf93
atman-lens <34531378+atman-lens@users.noreply.github.com>
2018-07-24 20:19:55 -0400

    Add components to app component

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>{props.username}</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quis. Sint vero perferendis delectus id nobis quis minima sed velit hic, tenetur, nihil eum iusto in est magni? Perspiciatis, sequi.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, fuga sint molestias atque ipsa quos, doloribus fugiat magni rerum praesentium repellendus veniam labore possimus rem id perferendis vitae nulla exercitationem.</p>
+        </div>
+    )
+};
+
+export default userOutput;
