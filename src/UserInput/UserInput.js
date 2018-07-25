4092f1fa484b0fc17fd3d448450b054f30066887
atman-lens <34531378+atman-lens@users.noreply.github.com>
2018-07-24 21:17:17 -0400

    Add styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const divStyle = {
+    textAlign: 'center'
+}
+
+const inputStyle = {
+    textAlign: 'center',
+    border: '2px solid black'
+}
+
 const userInput = (props) => {
     return (
-        <div className='UserInput'>
-            <input type='text' onChange={props.changed} value={props.username}/>
+        <div style={divStyle} className='UserInput'>
+            <input style={inputStyle} type='text' onChange={props.changed} value={props.username}/>
         </div>
     )
 };
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './UserOutput.css';
+
 
 const userOutput = (props) => {
     return (
         <div>
-            <p>{props.username}</p>
+            <p className='username'>{props.username}</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quis. Sint vero perferendis delectus id nobis quis minima sed velit hic, tenetur, nihil eum iusto in est magni? Perspiciatis, sequi.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, fuga sint molestias atque ipsa quos, doloribus fugiat magni rerum praesentium repellendus veniam labore possimus rem id perferendis vitae nulla exercitationem.</p>
         </div>
